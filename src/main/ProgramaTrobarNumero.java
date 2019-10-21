6bbec3bced42fe049daa95acdba99daceb31b03b
juanp_ksx3m2c <juanp_ksx3m2c@DESKTOP-O1H7D3O>
2019-10-21 19:02:28 +0200

    Volver despues de un tiempo8

--- src/main/ProgramaTrobarNumero.java
+++ src/main/ProgramaTrobarNumero.java
@@ -12,7 +12,9 @@ public class ProgramaTrobarNumero {
 		System.out.println("Introdueix el nombre que volem trobar repetit, ha de ser parell:");
 		int nValor = lector.nextInt();
 		
-		for(int i = 0; i < 500000; i++) {
+		
+		//Aquesta es la branca temps
+		for(int i = 0; i < 7000; i++) {
 			int numero = (int)(Math.random()*1000000+1);
 			if(nValor == numero) {
 				System.out.println("S'ha trobat el valor que busquem.");
